c6035da4d318c23940f9e7a98204f0e449d838db
diegolondono <diegolondono@MacBook-Pro-de-diegolondono.local>
2021-08-06 09:26:28 -0500

    configuracion general

---
+++ src/components/main.js
@@ -0,0 +1,42 @@
+import * as React from "react"
+
+// import "./main.css"
+
+
+const main = () => (
+    <div className="main">
+        <div className="content scroll">
+                <div className="center">
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+                <h3>¡Bienvenid@ a XXXXXXX!</h3>
+                <p>Encontremos al terapeuta y servicio más adecuado para tus necesidades. Responde unas preguntas sencillas para empezar. No toman más de 2 minutos.</p>
+                <button>Empecemos</button>
+            </div>
+        </div>
+    </div>
+)
+
+export default main
---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+// import "./sidebar.css"
+
+const sidebar = () => (
+  <div className="sidebar">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+)
+
+export default sidebar
